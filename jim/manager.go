b6dca2a6e4c2d8e843e049b57ad3d9af2e612106
Jesse Horne <j.horne2796@gmail.com>
2023-10-02 11:25:05 -0400

    implemented scrolling up and down with arrow keys

--- jim/manager.go
+++ jim/manager.go
@@ -3,7 +3,6 @@ package jim
 import (
 	"fmt"
 	"github.com/gdamore/tcell/v2"
-	"os"
 )
 
 type Manager struct {
@@ -21,26 +20,10 @@ func NewManager(s tcell.Screen) *Manager {
 }
 
 func (m *Manager) Init() {
-	w, h := m.Screen.Size()
-
 	// create folder/file viewer
 	m.Fv = NewFv(m.Screen)
 	m.Fv.ExpandDir(nil) // expand ./ in the main tree view
 	m.Fv.RefreshTree()
-
-	// create initial welcome tab
-	data, err := os.ReadFile("./welcome.txt")
-
-	if err != nil {
-		return
-	}
-	newTab := NewTab(m.Screen, m)
-	newTab.Width = w - m.Fv.Width
-	newTab.Height = h
-	newTab.OffsetX = m.Fv.WallX
-	newTab.OffsetY = 1
-	newTab.SetContent(string(data))
-	newTab.Redraw()
 
 	m.RedrawTabLabels()
 }
@@ -125,11 +108,6 @@ func (m *Manager) OpenTab(f *File) {
 	if t == nil {
 		w, h := m.Screen.Size()
 
-		data, err := os.ReadFile(f.FullPath)
-		if err != nil {
-			return
-		}
-
 		// open new tab and print content from 0 scroll y
 		newTab := NewTab(m.Screen, m)
 		newTab.Width = w - m.Fv.WallX
@@ -137,7 +115,7 @@ func (m *Manager) OpenTab(f *File) {
 		newTab.OffsetX = m.Fv.WallX
 		newTab.OffsetY = 1
 		newTab.File = f
-		newTab.SetContent(string(data))
+		newTab.LoadFile(f.FullPath)
 		newTab.Active = true
 		newTab.Redraw()
 
@@ -159,8 +137,6 @@ func (m *Manager) OpenTab(f *File) {
 		t.Redraw()
 		m.RedrawTabLabels()
 	}
-
-	// switch to current tab
 }
 
 func (m *Manager) ButtonEvent(x int, y int, buttons tcell.ButtonMask) {
@@ -187,6 +163,13 @@ func (m *Manager) ButtonEvent(x int, y int, buttons tcell.ButtonMask) {
 	}
 }
 
+func (m *Manager) MoveCursor(dir int) {
+	if m.LastActiveTab != nil {
+		m.LastActiveTab.MoveCursor(dir)
+		m.RedrawTabLabels()
+	}
+}
+
 func (m *Manager) HandleInput() {
 L:
 	for {
@@ -198,7 +181,16 @@ L:
 				break L
 			} else if ev.Key() == tcell.KeyCtrlW {
 				m.CloseTab(m.LastActiveTab)
+			} else if ev.Key() == tcell.KeyUp {
+				m.MoveCursor(CursorDirUp)
+			} else if ev.Key() == tcell.KeyDown {
+				m.MoveCursor(CursorDirDown)
+			} else if ev.Key() == tcell.KeyLeft {
+				m.MoveCursor(CursorDirLeft)
+			} else if ev.Key() == tcell.KeyRight {
+				m.MoveCursor(CursorDirRight)
 			}
+
 		case *tcell.EventMouse:
 			x, y := ev.Position()
 			buttons := ev.Buttons()
--- jim/tab.go
+++ jim/tab.go
@@ -2,22 +2,31 @@ package jim
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"os"
+)
+
+const (
+	CursorDirUp = iota
+	CursorDirDown
+	CursorDirLeft
+	CursorDirRight
 )
 
 type Tab struct {
-	Screen  tcell.Screen
-	Manager *Manager
-	File    *File
-	Active  bool
-	Width   int
-	Height  int
-	ScrollY int // the number of bytes scrolled down in the document
-	ScrollX int // the number of characters scrolled right
-	OffsetX int
-	OffsetY int
-	Content string
-	CursorX int
-	CursorY int
+	Screen    tcell.Screen
+	Manager   *Manager
+	File      *File
+	Active    bool
+	Width     int
+	Height    int
+	ScrollY   int // the number of bytes scrolled down in the document
+	ScrollX   int // the number of characters scrolled right
+	OffsetX   int
+	OffsetY   int
+	Content   []string
+	LineCount int
+	CursorX   int
+	CursorY   int
 }
 
 func NewTab(screen tcell.Screen, m *Manager) *Tab {
@@ -28,31 +37,72 @@ func NewTab(screen tcell.Screen, m *Manager) *Tab {
 }
 
 func (t *Tab) SetContent(s string) {
-	t.Content = s
+	var temp string
+	var lineCount int
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			t.Content = append(t.Content, temp)
+			temp = ""
+			lineCount++
+		} else {
+			temp = temp + string(s[i])
+		}
+	}
+	t.LineCount = lineCount + 1
 }
 
 func (t *Tab) LoadFile(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	t.SetContent(string(data))
+
 	return nil
 }
 
-func (t *Tab) Redraw() {
-	if t.ScrollY >= len(t.Content) {
-		return
+func (t *Tab) MoveCursor(dir int) {
+	if dir == CursorDirUp {
+		if t.CursorY > 0 {
+			if t.OffsetY < 1 {
+				t.CursorY--
+				t.OffsetY++
+				t.Redraw()
+			} else {
+				t.CursorY--
+			}
+		}
+	} else if dir == CursorDirDown {
+		if t.CursorY <= t.Height-3 {
+			t.CursorY++
+		} else {
+			if t.CursorY < len(t.Content) {
+				t.OffsetY--
+				t.CursorY++
+				t.Redraw()
+			}
+		}
 	}
 
+	t.Screen.ShowCursor(t.OffsetX+t.CursorX, t.OffsetY+t.CursorY)
+	t.Screen.Sync()
+}
+
+func (t *Tab) Redraw() {
 	t.Manager.ClearEditor()
 
 	style := tcell.StyleDefault.Background(ColorDark).Foreground(ColorWhite)
 
 	x, y := t.OffsetX, t.OffsetY
-	for _, c := range t.Content {
-		t.Screen.SetCell(x, y, style, c)
 
-		if c == '\n' {
-			y++
-			x = t.OffsetX
-		} else {
-			x++
+	for lineNumber := 0 - t.OffsetY; lineNumber < t.Height-t.OffsetY; lineNumber++ {
+		if lineNumber >= 0 && lineNumber < len(t.Content) {
+			line := t.Content[lineNumber]
+
+			for i := 0; i < len(line); i++ {
+				t.Screen.SetCell(x+i, y+lineNumber, style, rune(line[i]))
+			}
 		}
 	}
 
